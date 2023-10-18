15c8d86bd1ff183e9d55de0a92a2abcf68baa30a
Emil <emil.winberg1@gmail.com>
2023-10-18 15:31:51 +0200

    dynamic modals and cleanup

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 import data from "../files/education.json" assert { type: 'json'};
-const education = data['educations'];
-const jobs = data['jobs'];
 const menu = document.querySelector('.menu');
-const cards = document.querySelectorAll('.card');
 const menuItems = document.querySelectorAll('.menu-item');
 const hamburger = document.querySelector('.hamburger');
 const closeIcon = document.querySelector('.closeIcon');
@@ -23,80 +20,80 @@ function toggleMenu() {
 }
 hamburger.addEventListener('click', toggleMenu);
 
-// Render education-/job-cards
-function renderCards(occupation) {
-    var query = "." + occupation[0].type.toString();
-    var container = document.querySelector(query);
+const renderCards = ((data) => {
+    for(const occupation in data){
+        var query = "." + data[occupation][0].type.toString();
+        var container = document.querySelector(query);
+
+        for(const key in data[occupation]){
+            var card = document.createElement('div');
+            card.className = 'card';
+            card.id = data[occupation][key].id;
     
-    for(var i = 0; i < occupation.length; i++){
-
-        var card = document.createElement('div');
-        card.className = 'card';
-        card.id = occupation[i].id;
-
-        var title = document.createElement('h4');
-        title.innerHTML = occupation[i].name;
-
-        var img = document.createElement('img');
-        img.src = occupation[i].img;
-        img.classList.add(occupation[i].imgClass);
-        img.alt = occupation[i].name;
-
-        var a = document.createElement('a');
-        a.href = occupation[i].href;
-        a.setAttribute('data-toggle', 'modal');
-        a.target= "_blank"
-        a.appendChild(img);
-
-        card.appendChild(title);
-        card.appendChild(a)
-        container.appendChild(card);
+            var title = document.createElement('h4');
+            title.innerHTML = data[occupation][key].name;
+    
+            var img = document.createElement('img');
+            img.src = data[occupation][key].img;
+            img.classList.add(data[occupation][key].imgClass);
+            img.alt = data[occupation][key].name;
+    
+            card.appendChild(title);
+            card.appendChild(img);
+            container.appendChild(card);
+        }
     }
-}
-renderCards(education);
-renderCards(jobs);
-cards.forEach(btn => {
-    btn.addEventListener('click', toggleModal)
-})
+})(data);
 
+const renderModals = ((data) => {
+    for(var occupation in data){
+        console.log(occupation);
+        const modals = document.querySelector('.modals');
 
-function renderModals(education, jobs) {
-    for(var i = 0; i < (education.length + jobs.length); i++){
-        
+        for(var key in data[occupation]){
+            var modalCard = document.createElement('div');
+            modalCard.className = 'modalCard hidden';
+            modalCard.id = key;
+
+            var modalTitle = document.createElement('h3');
+            modalTitle.innerText = data[occupation][key].name;
+
+            var modalLogo = document.createElement('img');
+            modalLogo.src = data[occupation][key].img;
+            modalLogo.classList.add(data[occupation][key].imgClass);
+            modalLogo.alt = data[occupation][key].name;
+
+            var modalContent = document.createElement('div');
+            // modalContent.innerText = data[occupation][key].description;
+            modalContent.innerText = 'Heyheyhey!';
+
+            modalCard.appendChild(modalTitle);
+            console.log(modalTitle);
+
+            modalCard.appendChild(modalLogo);
+            console.log(modalLogo);
+
+            modalCard.appendChild(modalContent);
+            console.log(modalContent);
+
+            modals.appendChild(modalCard);
+        }
     }
-    var modalCard = document.createElement('div');
-    modalCard.className = 'modalCard hidden';
-    modalCard.id = card.id;
-
-    var modalTitle = document.createElement('h3');
-    modalTitle.textContent = 'Modal Title';
-
-    var modalContent = document.createElement('div');
-    modalContent.className = 'modalContent';
-
-    modalCard.appendChild(modalTitle);
-    modalCard.appendChild(modalContent);
-    card.appendChild(modalCard);
-}
-
-function toggleModal(e) {
-    if(e.target.classList.contains('hidden')){
-        e.target.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    } else {
-        e.target.classList.add('hidden');
-        overlay.classList.add('hidden');
-    }
-};
+})(data);
 
 
 
 
 
-
-
-
-
+// function toggleModal(e) {
+//     if(e.target.classList.contains('hidden')){
+//         e.target.classList.remove('hidden');
+//         overlay.classList.remove('hidden');
+//     } else {
+//         e.target.classList.add('hidden');
+//         overlay.classList.add('hidden');
+//     }
+// };
 
 
 // (async () => {
