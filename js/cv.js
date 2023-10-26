c88da474f8dacae2c673b6e7bd02e9250cb1b506
Emil <emil.winberg1@gmail.com>
2023-10-26 14:02:53 +0200

    Fix padding and margins on homepage

--- js/cv.js
+++ js/cv.js
@@ -100,8 +100,4 @@ const renderCards = ((data) => {
     }
 })(data);
 
-// (async () => {
-//     const response = await fetch('https://api.github.com/users/sbwinberg/repos');
-//     const repoJson = await response.json();
-//     console.log(repoJson);
-// })();
+
---
+++ js/home.js
@@ -0,0 +1,5 @@
+(async () => {
+    const response = await fetch('https://api.github.com/users/sbwinberg/repos');
+    const repoJson = await response.json();
+    console.log(repoJson);
+})();
