3f2436f75838e193ae6ed7e0185c0ed5a4e20fa5
Emil <emil.winberg1@gmail.com>
2023-10-30 14:35:34 +0100

    working carousel on mobile and desktop

--- js/home.js
+++ js/home.js
@@ -4,3 +4,34 @@
     console.log(repoJson);
 })();
 
+
+//CLICK AND DRAG SCROLL
+const slider = document.querySelector('.media-scroller');
+let isActive = false, prevPageX, prevScrollLeft;
+
+const dragStart = (e) => {
+    e.preventDefault();
+    isActive = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = slider.scrollLeft;
+    slider.classList.add('active');
+}
+const dragStop = () =>{
+    isActive = false;
+    slider.classList.remove('active');
+}
+const dragging = (e) => {
+    if(!isActive) return;
+    e.preventDefault();
+    let positionDiff = e.pageX - prevPageX;
+    slider.scrollLeft = prevScrollLeft - positionDiff;
+}
+const leave = () => {
+    slider.classList.remove('active');
+    isActive = false;
+}
+
+slider.addEventListener("mousedown", dragStart);
+slider.addEventListener("mouseup", dragStop);
+slider.addEventListener("mouseleave", leave);
+slider.addEventListener("mousemove", dragging);
