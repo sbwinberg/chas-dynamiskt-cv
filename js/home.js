502d2315ad05e258237a5d3c841181692d2d3a4d
Emil <emil.winberg1@gmail.com>
2023-10-31 14:54:40 +0100

    switch to inset values and code cleanup

--- js/home.js
+++ js/home.js
@@ -1,7 +1,8 @@
+// GITHUB REPO API
 (async () => {
     const response = await fetch('https://api.github.com/users/sbwinberg/repos');
-    const repoJson = await response.json();
-    console.log(repoJson);
+    const projects = await response.json();
+    console.log(projects);
 })();
 
 
@@ -35,3 +36,10 @@ slider.addEventListener("mousedown", dragStart);
 slider.addEventListener("mouseup", dragStop);
 slider.addEventListener("mouseleave", leave);
 slider.addEventListener("mousemove", dragging);
+
+
+//Render Project cards
+const renderCards = (projects) => {
+    const header = document.createElement('h2');
+    const description = document.createElement('p');
+}
