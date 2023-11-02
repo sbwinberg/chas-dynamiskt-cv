daced5246b42a7bbf009869443809362a8879bb7
Emil <emil.winberg1@gmail.com>
2023-11-02 12:00:58 +0100

    Sista finlir innan uppgiften släpps

--- js/default.js
+++ js/default.js
@@ -1,6 +1,14 @@
+// Dropdown-menu 
 const trigger = document.querySelector('.trigger');
 const nav = document.querySelector('.full-screen-nav');
 const backdrop = document.querySelector('.backdrop');
-
 trigger.addEventListener('click', () => nav.classList.add('open-nav'));
-backdrop.addEventListener('click', () => nav.classList.remove('open-nav'));
+backdrop.addEventListener('click', () => nav.classList.remove('open-nav')); 
+
+
+// Email-button functionality
+// const mailLink = document.getElementById('email');
+// function sendEmail() {
+//     window.location = "mailto:emil.winberg@chasacademy.se";
+// }
+// mailLink.addEventListener('click', sendEmail);
