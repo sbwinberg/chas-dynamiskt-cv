1aa35c4b735a619a089916ad9f15cd15629f753f
Emil <emil.winberg1@gmail.com>
2023-11-08 13:40:35 +0100

    Single responsibility js cardrender

--- js/home.js
+++ js/home.js
@@ -11,43 +11,84 @@ const setLink = (location) => {
     // link.appendChild(img);
     return link;
 }
+const createCard = () => {
+    const card = document.createElement('div');
+    card.className = 'card media-element';
+    return card
+}
+const createCardContent = () => {
+    const cardContent = document.createElement('div');
+    cardContent.className = 'card-content';
+    return cardContent;
+}
+const createCardBg = () => {
+    const bgContainer = document.createElement('div');
+    bgContainer.className = 'card-bg blur'
+    return bgContainer
+}
+// const setBgImg = () => {
+//     const cards = document.querySelectorAll('.card');
+//     for(let i = 0; i < cards.length; i++) {
+//         const bgContainer = document.createElement('div');
+//         if(i <= 10){
+//             let bgImg = `url('https://source.unsplash.com/featured/400x50${i}')`;
+//             bgContainer.style.background = `${bgImg}`;
+//         } else {
+//             let bgImg = `url('https://source.unsplash.com/featured/400x5${i}')`;
+//             bgContainer.style.background = `${bgImg}`;
+//         }
+//     }
+// }
+const renderCards = (projects) => {
+    const container = document.querySelector('.project-cards');
+    for(const project in projects){
+        // CREATE ELEMENTS
+        const card = createCard();
+        const cardContent = createCardContent();
+        const title = setTitle(projects[project]);
+        const link = setLink(projects[project])
+        const bg = createCardBg();
+        // APPEND
+        cardContent.appendChild(title);
+        cardContent.appendChild(link);
+        card.appendChild(bg);
+        card.appendChild(cardContent);
+        container.appendChild(card);
+    }
+}
+
+// function renderGalleryItem(randomNumber){
+//     const imageWidth = 450;    //image width in pixels
+//     const imageHeight = 500;   //image height in pixels
+//     const collectionID = 928423  //Beach & Coastal, the collection ID from the original url
+
+//     fetch(`https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomNumber}`)
+//       .then((response) => {
+//         let galleryItem = document.createElement('span');
+//         galleryItem.classList.add('gallery-item');
+//         galleryItem.innerHTML = `
+//           <img class="gallery-image" src="${response.url}" alt="gallery image"/>
+//           `
+//         galleryContainer.append(galleryItem);
+//     })
+// }
+
+// const renderBG = (cards) => {
+//     const cards = document.querySelectorAll('.card-bg');
+//     const numImagesAvailable = 988  //how many photos are total in the collection
+//     const numItemsToGenerate = cards.length; //how many photos you want to display
+
+//     for(let i=0; i < numItemsToGenerate; i++){
+//         let randomImageIndex = Math.floor(Math.random() * numImagesAvailable);
+//         renderGalleryItem(randomImageIndex);
+//     }
+// }
 
 (async () => {
     const response = await fetch('https://api.github.com/users/sbwinberg/repos');
     const projects = await response.json();
-    const container = document.querySelector('.project-cards');
-    for(const project in projects){
-        const card = document.createElement('div');
-        const cardContent = document.createElement('div');
-        cardContent.className = 'card-content';
-        card.className = 'card media-element';
+    renderCards(projects);
 
-        //  hover-frame till senare
-        const title = setTitle(projects[project]);
-        const link = setLink(projects[project])
-
-        cardContent.appendChild(title);
-        cardContent.appendChild(link);
-        card.appendChild(cardContent);
-        container.appendChild(card);
-    }
-
-    // const cards = document.querySelectorAll('.card');
-    // for(let i = 0; i < cards.length; i++) {
-    //     console.log(`https://unsplash.com/collections/592563/${i}`)
-    //     if(i <= 10){
-    //         let bgImg = `url('https://source.unsplash.com/featured/400x50${i}')`;
-    //         cards[i].style.background = `${bgImg}`;
-
-    //     } else {
-    //         let bgImg = `url('https://source.unsplash.com/featured/400x5${i}')`;
-    //         cards[i].style.background = `${bgImg}`;
-    //     }
-    //     cards[i].style.opacity = 1;
-    //     cards[i].style.backgroundRepeat = 'no-repeat';
-    //     cards[i].style.backgroundSize = 'cover';
-    //     cards[i].style.backgroundPosition = 'center';
-    // }
 })();
 
 //CLICK AND DRAG SCROLL
@@ -81,5 +122,3 @@ slider.addEventListener("mousedown", dragStart);
 slider.addEventListener("mouseup", dragStop);
 slider.addEventListener("mouseleave", leave);
 slider.addEventListener("mousemove", dragging);
-
-// PROJECT CARDS
